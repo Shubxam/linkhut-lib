8305efbb5c9e96455664974abb272e2d4d483db0
Shubham Soni <shubxam@gmail.com>
2025-07-05 08:07:48 +0530

    feat: enhance model validation and improve string representation for Tag, Date, and Bookmark classes

--- src/linkhut_lib/models.py
+++ src/linkhut_lib/models.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from pydantic import BaseModel, ConfigDict, Field, HttpUrl, field_validator
 
 from .exceptions import InvalidTagFormatError
+from .validation import validate_date, validate_tag
 
 
 class Tag(BaseModel):
@@ -19,22 +20,49 @@ class Tag(BaseModel):
     @classmethod
     def validate_name(cls, name: str) -> str:
         """Ensure tag name is alphanumeric and not empty."""
-        if not name.replace("-", "").replace("_", "").isalnum():
-            raise InvalidTagFormatError(f"Invalid characters present in tag name: {name}")
-        return name
+        return validate_tag(name)
 
     def __str__(self) -> str:
         return self.name
 
 
+class Date(BaseModel):
+    date: datetime = Field(
+        default_factory=datetime.now,
+        description="The date and time",
+    )
+
+    @field_validator("date")
+    @classmethod
+    def validate_date(cls, date: datetime | str) -> datetime:
+        """Ensure date is a valid datetime object or string."""
+        return validate_date(cls, date)
+        
+    def __str__(self) -> str:
+        return self.date.isoformat()
+
+
+class Url(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+    url: HttpUrl = Field(..., description="The URL of the bookmark", frozen=True)
+
+    def __str__(self) -> str:
+        return str(self.url)
+
+
 class Bookmark(BaseModel):
+    
     # without this, the model will not be able to use field names only alias names
     model_config = ConfigDict(populate_by_name=True)
 
-    url: HttpUrl = Field(..., description="The URL of the bookmark", frozen=True, alias="href")
+    url: Url = Field(
+        ...,
+        description="The URL of the bookmark",
+        alias="href",
+    )
     title: str = Field(..., description="The title of the bookmark", alias="description")
-    created_at: datetime = Field(
-        default_factory=datetime.now,
+    created_at: Date = Field(
+        default_factory=Date,
         description="The date and time when the bookmark was created",
         alias="time",
     )
@@ -63,7 +91,7 @@ class Bookmark(BaseModel):
         description="Whether the bookmark is marked as to-read",
     )
 
-    @field_validator("tags")
+    @field_validator("tags", mode="before")
     @classmethod
     def validate_tags(cls, tags: str | list[str] | list[Tag]) -> list[Tag]:
         """Create Tags List from string and Ensure tags are unique and not empty."""
@@ -84,10 +112,25 @@ class Bookmark(BaseModel):
             raise InvalidTagFormatError(
                 "Tags must be a string of space/comma/semicolon separated values, list of strings, or list of Tags."
             )
+    
+    @field_validator("created_at", mode="before")
+    @classmethod
+    def validate_created_at(cls, created_at: datetime) -> Date:
+        return Date(date=created_at)
+    
+    @field_validator("url", mode="before")
+    @classmethod
+    def validate_url(cls, url: str) -> Url:
+        return Url(url=url)  # type: ignore
 
     def __str__(self) -> str:
-        # TODO
-        return self.title
+        bookmark = f"""
+        Bookmark(title={self.title}
+        url={self.url.__str__()}
+        created_at={self.created_at.__str__()}
+        tags={[tag.__str__() for tag in self.tags]})
+        """
+        return bookmark.strip()
 
 
 class HTMLResponse(BaseModel):
@@ -96,9 +139,9 @@ class HTMLResponse(BaseModel):
     # allows assigning arbitrary types to fields, such as BeautifulSoup
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    content: BeautifulSoup = Field(..., description="Parsed HTML content")
+    content_soup: BeautifulSoup = Field(..., description="Parsed HTML content")
 
-    @field_validator("content")
+    @field_validator("content_soup")
     @classmethod
     def validate_content(cls, content: BeautifulSoup) -> BeautifulSoup:
         """Ensure content is a BeautifulSoup object."""
@@ -110,7 +153,7 @@ class HTMLResponse(BaseModel):
 class APIResponse(BaseModel):
     """Response model for JSON API responses."""
 
-    content: dict[str, Any] | list[dict[str, Any]] = Field(
+    content_json: dict[str, Any] | list[dict[str, Any]] = Field(
         ..., description="Structured JSON response data"
     )
 
